946dd525508d558d882214849344a2d90acdbffd
osnatashush <osnat@delivops.com>
2024-09-23 12:30:45 +0300

    add terraform version

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
 terraform {
+  required_version = ">= 0.13"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.30.0"
+      version = ">= 4.0.0"
     }
   }
 }
